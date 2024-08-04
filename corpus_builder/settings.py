a0bc498fa5fc35d8c63166230f462f6700be0806
murat <jumasheff@gmail.com>
2024-08-04 19:19:44 +0600

    Fix templates issue

--- corpus_builder/settings.py
+++ corpus_builder/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'corpus_builder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates' / 'corpus' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
